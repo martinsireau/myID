d2ce4e29e123fe653ec84ccb9a7e174467687f49
Martin SIREAU <martinsireau@Martins-MacBook-Pro.local>
2017-10-13 20:41:41 +0200

    init

---
+++ myID/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  d09
+//
+//  Created by Martin SIREAU on 10/13/17.
+//  Copyright © 2017 Martin SIREAU. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class ViewController: UIViewController {
+    
+    let context = LAContext()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
